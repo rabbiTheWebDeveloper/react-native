437929e477476c51a1d803121514f703224d634e
S M AL RABBI <rabbithedeveloper@gmail.com>
2024-04-14 07:58:03 +0600

    React Native Core Component
    React Native Core Component
    
    View
    Text
    Image image using source(retuire())
    Scroll View import ---all content making scrolling
    stype-seed
    styleSheet

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView  >
+      <Text style={{color: 'red'}}>Open up App.js to start working on your app!</Text>
+      <Text style={{color: 'white' ,backgroundColor:'red',  width: 200, height: 200, textAlign: 'center', fontSize: 80}}>Hello World</Text>
       <StatusBar style="auto" />
+      <Image source={{uri: "https://reactnative.dev/docs/assets/p_cat2.png"}} style={{width: 300, height: 300}} />
+      <Image source={{uri: "https://scontent.fjsr17-1.fna.fbcdn.net/v/t39.30808-6/435516938_1485945335655981_3391962969296622729_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeF8dM6eub4h7X8jvsS9cMbkomPz1H5KBViiY_PUfkoFWA-FUR7Wx1c5XfC6Hf-roKx6pr1WGHtmAq-ZMDQvW3bM&_nc_ohc=eXl8_Z22RRYAb4AoYZ7&_nc_oc=AdivfuLeObfpRCaWj0aN8yQ038mbzOvsx0wxLGnlOEXLoWhPFx5Bh5dXv_ATx6IY3nc&_nc_ht=scontent.fjsr17-1.fna&oh=00_AfAuncxWfxubIrOiVOcdgP3b7DZfuBc6x4Ihd8k5IcKvag&oe=6621040E"}} style={{width: 300, height: 300}} />
+      <Text style={{color: 'red'}}>Open up App.js to start working on your app!</Text>
+      <Text style={{color: 'white' ,backgroundColor:'red',  width: 200, height: 200, textAlign: 'center', fontSize: 80}}>Hello World</Text>
+      <StatusBar style="auto" />
+      <Image source={{uri: "https://reactnative.dev/docs/assets/p_cat2.png"}} style={{width: 300, height: 300}} />
+      <Image source={{uri: "https://scontent.fjsr17-1.fna.fbcdn.net/v/t39.30808-6/435516938_1485945335655981_3391962969296622729_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeF8dM6eub4h7X8jvsS9cMbkomPz1H5KBViiY_PUfkoFWA-FUR7Wx1c5XfC6Hf-roKx6pr1WGHtmAq-ZMDQvW3bM&_nc_ohc=eXl8_Z22RRYAb4AoYZ7&_nc_oc=AdivfuLeObfpRCaWj0aN8yQ038mbzOvsx0wxLGnlOEXLoWhPFx5Bh5dXv_ATx6IY3nc&_nc_ht=scontent.fjsr17-1.fna&oh=00_AfAuncxWfxubIrOiVOcdgP3b7DZfuBc6x4Ihd8k5IcKvag&oe=6621040E"}} style={{width: 300, height: 300}} />
+      <Text style={styles.text}>Footer</Text>
+      </ScrollView>
     </View>
   );
 }
@@ -17,4 +28,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'red',
+    backgroundColor: 'blue',
+    fontSize: 30
+  }
 });
